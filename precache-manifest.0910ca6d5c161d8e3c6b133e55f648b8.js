b9d32c7348fd34dc7cf345803cb9876bbc7efaed
Pedro <seromenho.pedro@gmail.com>
2018-12-31 15:48:17 +0100

    Updates

---
+++ precache-manifest.0910ca6d5c161d8e3c6b133e55f648b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b467e1b4182f43c2e180b39098aca55",
+    "url": "/gyuto/static/media/chevron-down-solid.6b467e1b.svg"
+  },
+  {
+    "revision": "991325cae252a2ff07ef",
+    "url": "/gyuto/static/js/runtime~main.991325ca.js"
+  },
+  {
+    "revision": "95a7190270fbf9bb572e",
+    "url": "/gyuto/static/js/main.95a71902.chunk.js"
+  },
+  {
+    "revision": "79adb2a56d13a2a5e1e6",
+    "url": "/gyuto/static/js/1.79adb2a5.chunk.js"
+  },
+  {
+    "revision": "95a7190270fbf9bb572e",
+    "url": "/gyuto/static/css/main.689d985e.chunk.css"
+  },
+  {
+    "revision": "79adb2a56d13a2a5e1e6",
+    "url": "/gyuto/static/css/1.294d6fde.chunk.css"
+  },
+  {
+    "revision": "91cee38cc8a067f5e48b591cad0e5d1a",
+    "url": "/gyuto/index.html"
+  }
+];
