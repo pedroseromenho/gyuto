56ef0efb2b7a9ab9d7339a933f30075e208ef2d2
Pedro <seromenho.pedro@gmail.com>
2019-01-05 23:55:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gyuto/precache-manifest.2c78790bca6464e4b93d2fe88c1113e6.js"
+  "/gyuto/precache-manifest.39a2b89743f4a572e2a7f5a36e86fe93.js"
 );
 
 workbox.clientsClaim();
